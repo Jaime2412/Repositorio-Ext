5fb92fade8ed1e38a6388c0dd4cb9132826c7866
Jaime2412 <jrt0013@alu.medac.es>
2024-05-07 19:48:56 +0200

    comit ficheros

---
+++ src/main/java/es/medac/tema19/Tema19.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.medac.tema19;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Tema19 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
